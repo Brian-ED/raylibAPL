b5a6aee5a931123f920b5e9394c9bd4bcb6609d1
Brian-ED <brianellingsgaard9@gmail.com>
2025-09-27 19:46:00 +0200

    Finally moved to using the proper shebang Now #!/usr/local/bin/dyalogscript Also fixed nix shell's auto-submodule install

--- nix/shell.nix
+++ nix/shell.nix
@@ -13,7 +13,7 @@ pkgs.mkShell {
     # Initialize submodule if c-header-to-bqn-ffi doesn't exist
     if [ ! -f imports/c-header-to-bqn-ffi/parse.bqn ]; then
       echo "Submodule c-header-to-bqn-ffi doesn't exist. Auto-creating it."
-      ${pkgs.git} submodule update --init --recursive
+      ${pkgs.git}/bin/git submodule update --init --recursive
     fi
   '';
 }
